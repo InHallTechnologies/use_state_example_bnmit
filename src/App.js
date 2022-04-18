192622bdd897584de4b34d708eb069198648e53d
InHall Technologies <inhall.updatebackup@gmail.com>
2022-04-18 20:48:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    console.log("I will be called on render and each re-render")
+  })
+
+  useEffect(() => {
+    console.log("I will be called only once. (only on render)");
+  }, [])
+
+  useEffect(() => {
+    console.log("I will be called on render and each time toggle (state variable) is changed")
+  }, [toggle])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={userInput} onChange={event => setUserInput(event.target.value)} />
+      <button onClick={_ => setToggle(!toggle)}>Toggle State</button>  
     </div>
   );
 }
